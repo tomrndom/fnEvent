dd8bc0ec5c1ba1c89fb4c911f77954948667f0f5
Tomás Castillo <tcastilloboireau@gmail.com>
2020-05-22 20:43:49 -0300

    fixed redux extension bug

--- src/state/ReduxWrapper.js
+++ src/state/ReduxWrapper.js
@@ -23,7 +23,7 @@ const persistedReducers = persistCombineReducers(config, {
   loggedUserState: loggedUserReducer,
 });
 
-const composeEnhancers = typeof window === 'object' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
 const store = createStore(persistedReducers, composeEnhancers(applyMiddleware(thunk)));
 
