1e3fc22c86e8889180ffd1bf217585ec47c9f51d
Tomás Castillo <tcastilloboireau@gmail.com>
2020-06-02 17:32:46 -0300

    rocket chat styling

--- src/components/RocketChat.js
+++ src/components/RocketChat.js
@@ -4,7 +4,7 @@ const RocketChatComponent = class extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { auth: '', loggedIn: false };
+    this.state = { auth: '', loggedIn: false, displayChat: false };
     this.getToken = this.getToken.bind(this);
   }
 
@@ -28,15 +28,20 @@ const RocketChatComponent = class extends React.Component {
 
   render() {
 
-    let { loggedIn } = this.state;
+    let { loggedIn, displayChat } = this.state;
 
     if (loggedIn) {
       return (
         <div className="rocket-chat">
+          <div className="rocket-chat--button" onClick={() => this.setState({ displayChat: !displayChat })}>
+            <img src={displayChat ? '/img/close.svg' : '/img/chat.svg'} width="30px"/>
+          </div>
           <iframe
             src="https://rocket-chat.dev.fnopen.com/channel/general?layout=embedded"
-            width='100%'
-            height="500px"
+            width='400px'
+            height="600px"
+            style={{display : `${displayChat? '': 'none'}`}}
+            className="rocket-chat--embedded"
           ></iframe>
         </div>
       )
