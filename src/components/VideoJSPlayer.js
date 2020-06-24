e9a135015b7ab7a521f3c73f539e9509000e8e69
Tomás Castillo <tcastilloboireau@gmail.com>
2020-06-24 11:15:15 -0300

    provisional videojs fix

--- src/components/VideoJSPlayer.js
+++ src/components/VideoJSPlayer.js
@@ -1,37 +1,61 @@
-import React from 'react';
+import React, { Component } from "react";
 import videojs from 'video.js';
 import Youtube from 'videojs-youtube';
 
 import 'video.js/dist/video-js.css'
 
-class VideoJSPlayer extends React.Component {
+export default class VideoPlayer extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      updateCount: 0
+    };
   }
-
-  componentDidMount() {
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-      console.log('onPlayerReady', this)
+  
+  setup() {
+    let updateCount = this.state.updateCount;
+    this.setState({
+      updateCount: updateCount + 1
     });
   }
 
-  componentWillUnmount() {
-    if (this.player) {
-      this.player.dispose()
+  componentDidMount() {
+    this.setup();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // You should probably change this check
+    if(this.props !== nextProps)
+      this.setup();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.updateCount !== prevState.updateCount) {
+      // If it has a player, dispose
+      if(this.player) {
+        this.player.dispose();
+      }
+      // Create new player
+      this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+        console.log('onPlayerReady', this)
+      });
     }
   }
 
+  componentWillUnmount() {
+    // Dispose player on unmount
+    if(this.player) {
+      this.player.dispose();
+    }
+  }
   render() {
-
+    // Use `key` so React knows this item is going to change
+    const key = `${this.props.id || ''}-${this.state.updateCount}`;
     return (
-      <div>
-        <div data-vjs-player>
-          <video ref={node => this.videoNode = node} className="video-js vjs-big-play-centered"></video>
-        </div>
+      <div key={key} data-vjs-player>
+        <video ref={ node => this.videoNode = node } className="video-js vjs-big-play-centered"></video>
       </div>
     )
   }
 }
-
-export default VideoJSPlayer;
