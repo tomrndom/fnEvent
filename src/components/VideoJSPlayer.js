f0508c57e9abb440eb43bf45ad821e274b5edf35
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2020-06-24 17:11:29 -0300

    Removes player dispose.
    Signed-off-by: Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>

--- src/components/VideoJSPlayer.js
+++ src/components/VideoJSPlayer.js
@@ -7,17 +7,13 @@ import 'video.js/dist/video-js.css'
 class VideoJSPlayer extends React.Component {
 
   constructor(props) {
-    super(props);
-    this.state = {
-      updateCount: 0
-    };
+    super(props)
+    this.state = { updateCount: 0 }
   }
   
   setup() {
-    let updateCount = this.state.updateCount;
-    this.setState({
-      updateCount: updateCount + 1
-    });
+    let updateCount = this.state.updateCount
+    this.setState({ updateCount: updateCount + 1 })
   }
 
   componentDidMount() {
@@ -25,28 +21,17 @@ class VideoJSPlayer extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props !== nextProps)
-      this.setup();
+    if (this.props !== nextProps) this.setup()
   }
 
   componentDidUpdate(prevProps, prevState) {
     if(this.state.updateCount !== prevState.updateCount) {
-      if(this.player) {
-        this.player.dispose();
-      }
-      this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-        console.log('onPlayerReady', this)
-      });
+      this.player = videojs(this.videoNode, this.props, null)
     }
   }
 
-  componentWillUnmount() {
-    if(this.player) {
-      this.player.dispose();
-    }
-  }
   render() {
-    const key = `${this.props.id || ''}-${this.state.updateCount}`;
+    const key = `${this.props.id || ''}-${this.state.updateCount}`
     return (
       <div key={key} data-vjs-player>
         <video ref={ node => this.videoNode = node } className="video-js vjs-big-play-centered"></video>
