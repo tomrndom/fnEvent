e087aa4e7a3b5440390d533ca25e86b25d4ef931
Tomás Castillo <tcastilloboireau@gmail.com>
2020-07-01 15:48:52 -0300

    new home layout

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -36,26 +36,26 @@ export const HomePageTemplate = class extends React.Component {
     const { loggedUser, summit } = this.props;
 
     return (
-      <div className="container pt-5 pb-5">
+      <div className="px-5 py-5 mb-6">
         <div className="columns">
-          <div className="column is-three-quarters">
-            <div className="rocket-container">
-              {summit && <h2>Welcome to {summit.name}</h2>}
-              <br />
-              <ScheduleLiteClientSide accessToken={loggedUser.accessToken} eventClick={(ev) => this.onEventChange(ev)} />
-            </div>
-            <div>
-              <LiveEventWidgetClientSide
-                  summitId={summit.id}
-                  apiBaseUrl={`${typeof window === 'object' ? window.SUMMIT_API_BASE_URL : process.env.GATSBY_SUMMIT_API_BASE_URL}`}
-                  marketingApiBaseUrl={`${typeof window === 'object' ? window.MARKETING_API_BASE_URL : process.env.GATSBY_MARKETING_API_BASE_URL}`}
-              />
-            </div>
-          </div>
-          <div className="column is-one-quarter pb-6">
+          <div className="column is-one-quarter">
+            <h2>Community</h2>
             <div className="sponsor-container">
               <img src="/img/intel.png" alt="sponsor" />
             </div>
+          </div>
+          <div className="column is-half">
+            {summit && <h2>Welcome to {summit.name}</h2>}
+            <br />
+            <LiveEventWidgetClientSide
+              summitId={summit.id}
+              apiBaseUrl={`${typeof window === 'object' ? window.SUMMIT_API_BASE_URL : process.env.GATSBY_SUMMIT_API_BASE_URL}`}
+              marketingApiBaseUrl={`${typeof window === 'object' ? window.MARKETING_API_BASE_URL : process.env.GATSBY_MARKETING_API_BASE_URL}`}
+            />            
+          </div>
+          <div className="column is-one-quarter pb-6">
+            <h2>My Info</h2>
+            <ScheduleLiteClientSide accessToken={loggedUser.accessToken} eventClick={(ev) => this.onEventChange(ev)} />            
           </div>
         </div>
       </div>
