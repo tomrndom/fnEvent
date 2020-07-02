70fe697eb0895d48860bf6c95e0992387427a567
Tomás Castillo <tcastilloboireau@gmail.com>
2020-07-02 16:13:05 -0300

    new modular footer, with content from json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,77 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { StaticQuery, graphql } from "gatsby"
 
-const Footer = class extends React.Component {
-  render() {
-    return (
+import footerContent from '../content/footer.json';
+
+import styles from '../styles/footer.module.scss';
+
+const Footer = ({ summit }) => (
+
+  <StaticQuery
+    query={graphql`
+        query FooterQuery {
+          summit {
+            logo
+          }
+        }
+      `}
+    render={data => (
       <footer className="footer">
-        <div className="has-text-centered">
-          © 2020 All Rights Reserved.
+        <div className="columns">
+          <div className="column is-one-fifth">
+            {footerContent.logo.display &&
+              <img src={summit && summit.logo ?
+                summit.logo
+                :
+                data.summit && data.summit.logo ?
+                  data.summit.logo
+                  :
+                  null
+              } />
+            }
+          </div>
+        </div>
+        <div className={`${styles.footerColummns} columns`}>
+          <div className="column is-three-quarter">
+            <div className="columns">
+              {footerContent.columns.map((col, index) => {
+                return (
+                  <div className={`column is-3 ${index > 0 ? 'is-offset-1' : ''}`} key={index}>
+                    <h3>
+                      {col.title}
+                    </h3>
+                    {col.items.map((item, index) => (
+                      <a href={item.link} key={index}>
+                        <h4>{item.title}</h4>
+                      </a>
+                    ))}
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+
+          <div className="column is-one-quarter">
+            <h3>{footerContent.social.title}</h3>
+            <div className={styles.socialContainer}>
+              {footerContent.social.networks.map((net, index) => (
+                net.display &&
+                <a href={net.link} key={index}>
+                  <i className={`fa icon is-large ${net.icon}`}></i>
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
       </footer>
-    )
-  }
-}
+    )}
+  />
 
-export default Footer
+)
+
+const mapStateToProps = ({ summitState }) => ({
+  summit: summitState.summit
+})
+
+export default connect(mapStateToProps)(Footer)
