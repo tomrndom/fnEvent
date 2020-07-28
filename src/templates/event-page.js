2995f18b573d95d644accc84365446e5a45aff18
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2020-07-28 12:33:56 -0300

    Cleans up event page template.

--- src/templates/event-page.js
+++ src/templates/event-page.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { navigate } from 'gatsby'
 import { connect } from 'react-redux'
+
+import envVariables from '../utils/envVariables';
+
 import Layout from '../components/Layout'
 
 import DisqusComponent from '../components/DisqusComponent'
@@ -22,7 +25,6 @@ export const EventPageTemplate = class extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.onEventChange = this.onEventChange.bind(this);
     this.getMaterials = this.getMaterials.bind(this);
   }
@@ -33,15 +35,8 @@ export const EventPageTemplate = class extends React.Component {
   }
 
   componentDidMount() {
-    const { loggedUser } = this.props;
-    if (loggedUser.isLoggedUser) {
-      this.props.getDisqusSSO();
-      this.props.getRocketChatSSO();
-    }
-  }
-
-  componentDidUpdate() {
-
+    this.props.getDisqusSSO();
+    this.props.getRocketChatSSO();
   }
 
   onEventChange(ev) {    
@@ -59,107 +54,95 @@ export const EventPageTemplate = class extends React.Component {
 
   render() {
 
-    const { loggedUser, event, summit, user } = this.props;
+    const { loggedUser, summit, event, user } = this.props;
 
-    if (loggedUser) {
-      if (event) {
-        return (
-          <>
-            {event.id &&
-              <AttendanceTracker
-                key={event.id}
-                eventId={event.id}
-                summitId={summit.id}
-                apiBaseUrl={process.env.GATSBY_SUMMIT_API_BASE_URL}
-                accessToken={loggedUser.accessToken}
-              />
-            }
-            <section className="section px-0 py-0">
-              <div className="columns is-gapless">
-                {event.streaming_url ?
-                  <div className="column is-three-quarters px-0 py-0">
-                    <VideoComponent url={event.streaming_url} />
-                  </div>
-                  :
-                  <div className="column is-three-quarters px-0 py-0 is-hidden-mobile">
-                    <TalkComponent event={event} summit={summit} noStream={true} />
-                  </div>
-                }
-                <div className="column is-hidden-tablet">
+    if (event) {
+      return (
+        <>
+          {event.id &&
+            <AttendanceTracker
+              key={event.id}
+              eventId={event.id}
+              summitId={summit.id}
+              apiBaseUrl={envVariables.SUMMIT_API_BASE_URL}
+              accessToken={loggedUser.accessToken}
+            />
+          }
+          <section className="section px-0 py-0">
+            <div className="columns is-gapless">
+              {event.streaming_url ?
+                <div className="column is-three-quarters px-0 py-0">
+                  <VideoComponent url={event.streaming_url} />
+                </div>
+                :
+                <div className="column is-three-quarters px-0 py-0 is-hidden-mobile">
                   <TalkComponent event={event} summit={summit} noStream={true} />
                 </div>
-                <div className="column" style={{ position: 'relative' }}>
-                  <DisqusComponent disqusSSO={user.disqusSSO} event={event} summit={summit} title="Join the conversation" />
+              }
+              <div className="column is-hidden-tablet">
+                <TalkComponent event={event} summit={summit} noStream={true} />
+              </div>
+              <div className="column" style={{ position: 'relative' }}>
+                <DisqusComponent disqusSSO={user.disqusSSO} event={event} summit={summit} title="Join the conversation" />
+              </div>
+            </div>
+          </section>
+          {event.streaming_url &&
+            <section className="section px-0 py-0">
+              <div className="columns mx-0 my-0">
+                <div className="column px-0 py-0 is-three-quarters is-hidden-mobile">
+                  <TalkComponent event={event} summit={summit} noStream={true} />
                 </div>
               </div>
             </section>
-            {event.streaming_url &&
-              <section className="section px-0 py-0">
-                <div className="columns mx-0 my-0">
-                  <div className="column px-0 py-0 is-three-quarters is-hidden-mobile">
-                    <TalkComponent event={event} summit={summit} noStream={true} />
-                  </div>
-                </div>
-              </section>
-            }
-            {event.etherpad_link &&
-              <section className="section px-4 py-6">
-                <div className="columns">
-                  <div className="column is-three-quarters">
-                    <Etherpad className="talk__etherpad" etherpad_link={event.etherpad_link + `&userName=${user.userProfile.first_name}`} />
-                  </div>
-                  <div className="column is-one-quarter">
-                  </div>
-                </div>
-              </section>
-            }
+          }
+          {event.etherpad_link &&
             <section className="section px-4 py-6">
               <div className="columns">
-                <div className="column is-one-quarter pb-6">
-                  <AdvertiseComponent section='event' column="left" />
+                <div className="column is-three-quarters">
+                  <Etherpad className="talk__etherpad" etherpad_link={event.etherpad_link + `&userName=${user.userProfile.first_name}`} />
                 </div>
-                <div className="column is-two-quarters pb-6">
-                  {/* <div className="rocket-container"> */}
-                  <ScheduleLiteComponent accessToken={loggedUser.accessToken} landscape={true} eventClick={(ev) => this.onEventChange(ev)} />
-                  {/* <RocketChatComponent rocketChatSSO={user.rocketChatSSO} embedded={false} /> */}
-                  {/* </div> */}
+                <div className="column is-one-quarter">
                 </div>
-                <DocumentsComponent materials={this.getMaterials(event)} />
               </div>
-            </section >
-          </>
-        )
-      } else {
-        return (
+            </section>
+          }
           <section className="section px-4 py-6">
             <div className="columns">
-              <div className="column is-three-quarters pb-6">
+              <div className="column is-one-quarter pb-6">
+                <AdvertiseComponent section='event' column="left" />
+              </div>
+              <div className="column is-two-quarters pb-6">
                 {/* <div className="rocket-container"> */}
-                <span>Event not found</span>
-                <br />
                 <ScheduleLiteComponent accessToken={loggedUser.accessToken} landscape={true} eventClick={(ev) => this.onEventChange(ev)} />
-                {/*   <RocketChatComponent accessToken={loggedUser.accessToken} embedded={false} /> */}
+                {/* <RocketChatComponent rocketChatSSO={user.rocketChatSSO} embedded={false} /> */}
                 {/* </div> */}
               </div>
-              <div className="column is-one-quarter has-text-centered pb-6">
-                <AdvertiseComponent section='event' id={event.id} />
-              </div>
+              <DocumentsComponent materials={this.getMaterials(event)} />
             </div>
           </section >
-        )
-      }
+        </>
+      )
+    } else {
+      return (
+        <section className="section px-4 py-6">
+          <div className="columns">
+            <div className="column is-three-quarters pb-6">
+              {/* <div className="rocket-container"> */}
+              <span>Event not found</span>
+              <br />
+              <ScheduleLiteComponent accessToken={loggedUser.accessToken} landscape={true} eventClick={(ev) => this.onEventChange(ev)} />
+              {/*   <RocketChatComponent accessToken={loggedUser.accessToken} embedded={false} /> */}
+              {/* </div> */}
+            </div>
+            <div className="column is-one-quarter has-text-centered pb-6">
+              <AdvertiseComponent section='event' id={event.id} />
+            </div>
+          </div>
+        </section >
+      )
     }
   }
-}
-
-EventPageTemplate.propTypes = {
-  loggedUser: PropTypes.object,
-  event: PropTypes.object,
-  user: PropTypes.object,
-  eventId: PropTypes.string,
-  getEventBySlug: PropTypes.func,
-  getDisqusSSO: PropTypes.func,
-  getRocketChatSSO: PropTypes.func
 }
 
 const EventPage = (
@@ -168,7 +151,6 @@ const EventPage = (
     summit,
     event,
     eventId,
-    location,
     user,
     getEventBySlug,
     getDisqusSSO,
@@ -177,13 +159,12 @@ const EventPage = (
 ) => {
 
   return (
-    <Layout location={location}>
+    <Layout>
       <EventPageTemplate
         loggedUser={loggedUser}
-        event={event}
         summit={summit}
+        event={event}
         eventId={eventId}
-        location={location}
         user={user}
         getEventBySlug={getEventBySlug}
         getDisqusSSO={getDisqusSSO}
@@ -195,20 +176,38 @@ const EventPage = (
 
 EventPage.propTypes = {
   loggedUser: PropTypes.object,
-  location: PropTypes.object,
-  event: PropTypes.object,
   summit: PropTypes.object,
-  user: PropTypes.object,
+  event: PropTypes.object,
   eventId: PropTypes.string,
+  user: PropTypes.object,
   getEventBySlug: PropTypes.func,
   getDisqusSSO: PropTypes.func,
   getRocketChatSSO: PropTypes.func
 }
 
-const mapStateToProps = ({ loggedUserState, eventState, summitState, userState }) => ({
+EventPageTemplate.propTypes = {
+  loggedUser: PropTypes.object,
+  summit: PropTypes.object,
+  event: PropTypes.object,
+  eventId: PropTypes.string,
+  user: PropTypes.object,
+  getEventBySlug: PropTypes.func,
+  getDisqusSSO: PropTypes.func,
+  getRocketChatSSO: PropTypes.func
+}
+
+const mapStateToProps = (
+  {
+    loggedUserState,
+    summitState,
+    eventState,
+    userState
+  }
+) => ({
+
   loggedUser: loggedUserState,
-  event: eventState.event,
   summit: summitState.summit,
+  event: eventState.event,
   user: userState,
 })
 
