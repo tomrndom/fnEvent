21db42928cd09f5e846ea7c2d0c230fcf8f788b9
Santi <santipalenque@gmail.com>
2020-07-30 16:50:37 -0300

    add oneventclick

--- src/pages/a.js
+++ src/pages/a.js
@@ -5,8 +5,10 @@ import { connect } from 'react-redux'
 import LoginPage from "../templates/login-page"
 import HomePage from "../templates/home-page"
 import EventPage from "../templates/event-page"
+import SchedulePage from "../templates/schedule-page";
 
 import PrivateRoute from "../routes/PrivateRoute"
+import PublicRoute from "../routes/PublicRoute"
 
 const App = ({ isLoggedUser }) => (
   <Location>
@@ -14,6 +16,7 @@ const App = ({ isLoggedUser }) => (
       <Router basepath="/a" >
         <PrivateRoute path="/" component={HomePage} isLoggedIn={isLoggedUser} location={location} />
         <PrivateRoute path="/event/:eventId" component={EventPage} isLoggedIn={isLoggedUser} location={location} />
+        <PublicRoute path="/schedule" component={SchedulePage} location={location} />
         <LoginPage path="/login" location={location} />
       </Router>
     )}
---
+++ src/templates/schedule-page.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { navigate } from 'gatsby'
+import { connect } from 'react-redux'
+import Layout from '../components/Layout'
+import ScheduleLiteComponent from "../components/ScheduleLiteComponent";
+
+
+const SchedulePage = ({summit, loggedUser}) => {
+
+  return (
+    <Layout>
+      <div className="container">
+        <h1>Schedule</h1>
+        <hr/>
+        <ScheduleLiteComponent
+            accessToken={loggedUser.accessToken}
+            landscape={true}
+            showNav={true}
+            showFilters={true}
+            eventCount={100}
+            eventClick={ev => navigate(`/a/event/${ev}`)}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+SchedulePage.propTypes = {
+  location: PropTypes.object,
+}
+
+const mapStateToProps = ({ loggedUserState, summitState, userState }) => ({
+  loggedUser: loggedUserState,
+  summit: summitState.summit,
+  user: userState,
+});
+
+export default connect(
+  mapStateToProps, {}
+)(SchedulePage);
