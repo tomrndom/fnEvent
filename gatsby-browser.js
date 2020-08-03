f26be89379983b95b9911c3916c12d9c6f31ac60
Tomás Castillo <tcastilloboireau@gmail.com>
2020-08-03 12:33:11 -0300

    updating colors data

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,30 +1,21 @@
 import ReduxWrapper from "./src/state/ReduxWrapper"
+import Colors from './src/gatsby-plugin-theme-ui'
+
 export const wrapRootElement = ReduxWrapper
 
-const axios = require('axios')
-
 if (typeof window === 'object') {
-  window.IDP_BASE_URL               = process.env.GATSBY_IDP_BASE_URL;
-  window.SUMMIT_API_BASE_URL        = process.env.GATSBY_SUMMIT_API_BASE_URL;
-  window.SUMMIT_ID                  = process.env.GATSBY_SUMMIT_ID;
-  window.OAUTH2_CLIENT_ID           = process.env.GATSBY_OAUTH2_CLIENT_ID;
-  window.SCOPES                     = process.env.GATSBY_SCOPES;
-  window.MARKETING_API_BASE_URL     = process.env.GATSBY_MARKETING_API_BASE_URL;
+  window.IDP_BASE_URL = process.env.GATSBY_IDP_BASE_URL;
+  window.SUMMIT_API_BASE_URL = process.env.GATSBY_SUMMIT_API_BASE_URL;
+  window.SUMMIT_ID = process.env.GATSBY_SUMMIT_ID;
+  window.OAUTH2_CLIENT_ID = process.env.GATSBY_OAUTH2_CLIENT_ID;
+  window.SCOPES = process.env.GATSBY_SCOPES;
+  window.MARKETING_API_BASE_URL = process.env.GATSBY_MARKETING_API_BASE_URL;
 
-  let params = {
-    per_page: 100,
-    page: 1
-  };
-
-  axios.get(
-    `${process.env.GATSBY_MARKETING_API_BASE_URL}/api/public/v1/config-values/all/shows/${process.env.GATSBY_SUMMIT_ID}`, { params }
-  ).then((response) => {    
-    response.data.data.forEach(setting => {      
-      if (getComputedStyle(document.documentElement).getPropertyValue(`--${setting.key}`)) {
-          document.documentElement.style.setProperty(`--${setting.key}`, setting.value);
-          document.documentElement.style.setProperty(`--${setting.key}50`, `${setting.value}50`);
-      }
+  Object.entries(Colors.colors).map(color => {    
+    if (getComputedStyle(document.documentElement).getPropertyValue(`--${color[0]}`)) {      
+      document.documentElement.style.setProperty(`--${color[0]}`, color[1]);
+      document.documentElement.style.setProperty(`--${color[0]}50`, `${color[1]}50`);
+    }
   });
-  }).catch(e => console.log('ERROR: ', e));  
 }
 
