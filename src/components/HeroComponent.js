4bf187e5804fc5e6a02253af61141bfe285ddbf5
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2020-08-13 16:10:35 -0300

    Fixes hero component styles.

--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -3,9 +3,9 @@ import React from "react"
 const HeroComponent = ({ title, subtitle, event }) => {
 
   return (
-    <section className={`hero ${event ? 'talk__break' : 'is-fullheight'}`}>
+    <section className={`hero is-fullheight ${event ? 'talk__break' : ''}`}>
       <div className="hero-body">
-        <div className={`container ${event ? '' : 'has-text-centered'}`}>
+        <div className={`${event ? '' : 'container has-text-centered'}`}>
           <h1 className="title">{title}</h1>
           <h2 className="subtitle">{subtitle}</h2>
         </div>
