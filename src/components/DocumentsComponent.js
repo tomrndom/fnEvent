35a2a2e4d0b5b61944ee3ea801e686b803fcdbcb
Tomás Castillo <tcastilloboireau@gmail.com>
2020-08-14 15:34:13 -0300

    document component fixes

--- src/components/DocumentsComponent.js
+++ src/components/DocumentsComponent.js
@@ -23,7 +23,7 @@ const DocumentsComponent = ({ event }) => {
 
   if (sortedMaterials.length > 0) {
     return (
-      <div className="column is-one-quarters">
+      <div className="column is-one-quarter">
         <div className={`${styles.docsContainer}`} style={{ marginTop: '1em' }}>
           <div className={styles.title}>Documents</div>
           <hr />
@@ -35,11 +35,11 @@ const DocumentsComponent = ({ event }) => {
                       <div className="column is-2 is-offset-1">
                         <i className={`fa fa-file-o icon is-large`}></i>
                       </div>
-                      <div className="column is 6">
+                      <div className={`column is-6 ${styles.documentName}`}>
                         <span><b>{material.name}</b></span>
                       </div>
                       <div className="column is-2 has-text-right">
-                        <a href={material.link}><i className="fa fa-download icon is-large"></i></a>
+                        <a target="_blank" rel="noreferrer" href={material.link}><i className="fa fa-download icon is-large"></i></a>
                       </div>
                       <div className="column is-1"></div>
                     </div>
@@ -48,8 +48,8 @@ const DocumentsComponent = ({ event }) => {
                       <div className="column is-2 is-offset-1">
                         <i className={`fa ${material.class_name === 'PresentationVideo' ? 'fa-video-camera' : 'fa-link'} icon is-large`}></i>
                       </div>
-                      <div className="column is 8">
-                        <a href={material.class_name === 'PresentationVideo' ? `https://www.youtube.com/watch?v=${material.youtube_id}` : material.link} target="_blank" rel="noreferrer">
+                      <div className="column is-8">
+                        <a target="_blank" rel="noreferrer" href={material.class_name === 'PresentationVideo' ? `https://www.youtube.com/watch?v=${material.youtube_id}` : material.link}>
                           <span><b>{material.name}</b></span>
                         </a>
                       </div>
