d9d529936270d04e1a98c79ad944a87b9b0f1457
Tomás Castillo <tcastilloboireau@gmail.com>
2020-08-25 15:03:48 -0300

    fix virtual booth preview in cms

--- src/cms/preview-templates/VirtualBoothPagePreview.js
+++ src/cms/preview-templates/VirtualBoothPagePreview.js
@@ -3,15 +3,34 @@ import PropTypes from 'prop-types'
 import { VirtualBoothPageTemplate } from '../../templates/virtual-booth-page'
 
 const VirtualBoothPagePreview = ({ entry, getAsset, widgetFor }) => {
-  return (
-    <VirtualBoothPageTemplate
-      title={entry.getIn(['data', 'title'])}
-      intro={entry.getIn(['data', 'intro'])}
-      columns={entry.getIn(['data', 'columns'])}
-      endText={entry.getIn(['data', 'endText'])}
-      content={widgetFor('body')}
-    />
-  )
+
+  const data = entry.getIn(['data']).toJS()
+
+  if (data) {
+    return (
+      <VirtualBoothPageTemplate
+        title={entry.getIn(['data', 'title'])}
+        intro={entry.getIn(['data', 'intro'])}
+        columns={{
+          leftColumn: {
+            title: entry.getIn(['data', 'columns', 'leftColumn', 'title']),
+            content: entry.getIn(['data', 'columns', 'leftColumn', 'content']),
+            image: entry.getIn(['data', 'columns', 'leftColumn', 'image'])
+          },
+          rightColumn: {
+            title: entry.getIn(['data', 'columns', 'rightColumn', 'title']),
+            content: entry.getIn(['data', 'columns', 'rightColumn', 'content']),
+            image: entry.getIn(['data', 'columns', 'rightColumn', 'image'])
+          }
+        }}
+        endText={entry.getIn(['data', 'endText'])}
+        content={widgetFor('body')}
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+
 }
 
 VirtualBoothPagePreview.propTypes = {
