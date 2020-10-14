c779570ba5c29f04abf102c5d5c387b590244a65
Tomás Castillo <tcastilloboireau@gmail.com>
2020-10-13 21:20:23 -0300

    adjusting header in sponsors page

--- src/components/SponsorHeader.js
+++ src/components/SponsorHeader.js
@@ -6,11 +6,18 @@ import styles from '../styles/sponsor-page.module.scss'
 
 const SponsorHeader = ({ sponsor, tier }) => (
   <section className={styles.hero}>
-    <div className={`${styles.heroSponsor}`} style={{ backgroundImage: `url(${sponsor.headerImage})` }}>
-      <video className={`${styles.heroVideo}`} preload="auto" autoPlay loop muted="muted" volume="0">
-        <source src={sponsor.headerVideo} type="video/mp4" />
-      </video>
-      <div className={`${styles.heroBody}`} style={{ height: `${tier.sponsorPage.sponsorTemplate !== 'big-header' ? '250px' : ''}` }}>
+    <div className={`${styles.heroSponsor}`}
+      style={{
+        backgroundImage: `url(${sponsor.headerImage})`,
+        paddingBottom: `${tier.sponsorPage.sponsorTemplate === 'big-header' ? '27.77%' : '13.88%'}`,
+        maxHeight: `${tier.sponsorPage.sponsorTemplate === 'big-header' ? '400px' : '200px'}`
+      }}>
+      {!sponsor.headerVideo &&
+        <video className={`${styles.heroVideo}`} preload="auto" autoPlay loop muted="muted" volume="0">
+          <source src={sponsor.headerVideo} type="video/mp4" />
+        </video>
+      }
+      <div className={`${styles.heroBody}`}>
         <div className={`${styles.heroSponsorContainer}`}>
           <div className={styles.leftContainer}>
             {sponsor.socialNetworks && sponsor.socialNetworks.map((net, index) => (
@@ -24,16 +31,16 @@ const SponsorHeader = ({ sponsor, tier }) => (
             <div className={styles.category}>
               <img src={tier.badge} />
             </div>
-            <div className={styles.buttons}>
+            <div className={`${tier.sponsorPage.sponsorTemplate === 'big-header' ? styles.buttons : styles.buttonsSmall}`}>
               <Link className={styles.link}>
-                <button className={`${styles.button} button is-large`} style={{backgroundColor: `${sponsor.sponsorColor}`}}>
+                <button className={`${styles.button} button is-large`} style={{ backgroundColor: `${sponsor.sponsorColor}` }}>
                   <i className={`fa fa-2x fa-qrcode icon is-large`}></i>
                   <b>Scan your badge</b>
                 </button>
               </Link>
               {sponsor.email &&
                 <Link className={styles.link} to={`mailto:${sponsor.email}`}>
-                  <button className={`${styles.button} button is-large`} style={{backgroundColor: `${sponsor.sponsorColor}`}}>
+                  <button className={`${styles.button} button is-large`} style={{ backgroundColor: `${sponsor.sponsorColor}` }}>
                     <i className={`fa fa-2x fa-envelope icon is-large`}></i>
                     <b>Contact Us!</b>
                   </button>
@@ -44,7 +51,7 @@ const SponsorHeader = ({ sponsor, tier }) => (
         </div>
       </div>
     </div>
-    <div className={styles.bottomBar} style={{backgroundColor: `${sponsor.sponsorColor}`}}>
+    <div className={styles.bottomBar} style={{ backgroundColor: `${sponsor.sponsorColor}` }}>
       <div className={styles.track}>
         <div>
           {`${sponsor.marquee} / `.repeat(100).slice(0, 259)}
