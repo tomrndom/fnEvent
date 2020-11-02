112e289c318e30ed58a473b74299691ae0c2cd1e
Tomás Castillo <tcastilloboireau@gmail.com>
2020-11-02 16:32:17 -0300

    replace var for let

--- src/utils/uuidGenerator.js
+++ src/utils/uuidGenerator.js
@@ -1,7 +1,7 @@
 export const create_UUID = () => {
-    var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        var r = (dt + Math.random() * 16) % 16 | 0;
+    let dt = new Date().getTime();
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        let r = (dt + Math.random() * 16) % 16 | 0;
         dt = Math.floor(dt / 16);
         return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
