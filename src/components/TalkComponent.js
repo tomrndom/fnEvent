d5eac30866a0eada926e564d11240fe4c09b55b7
Tomás Castillo <tcastilloboireau@gmail.com>
2020-11-03 19:52:21 -0300

    Add speaker's company to event description

--- src/components/TalkComponent.js
+++ src/components/TalkComponent.js
@@ -67,7 +67,8 @@ const TalkComponent = class extends React.Component {
                         <div>
                           {`${s.first_name} ${s.last_name}`}
                           <br />
-                          <b dangerouslySetInnerHTML={{ __html: s.title }} />
+                          {s.title && <b dangerouslySetInnerHTML={{ __html: s.title }} />}
+                          {s.company && <b> - {s.company}</b>}
                         </div>
                       </div>
                     )
