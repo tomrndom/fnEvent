08a7d321531c4c093c1fbacc3b0d50be32f3e708
Tomás Castillo <tcastilloboireau@gmail.com>
2020-12-11 16:19:45 -0300

    fixing empty input fields for country and language

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,7 @@ export const wrapRootElement = ReduxWrapper
 
 window.IDP_BASE_URL = process.env.GATSBY_IDP_BASE_URL;
 window.SUMMIT_API_BASE_URL = process.env.GATSBY_SUMMIT_API_BASE_URL;
+window.API_BASE_URL = process.env.GATSBY_SUMMIT_API_BASE_URL;
 window.SUMMIT_ID = process.env.GATSBY_SUMMIT_ID;
 window.OAUTH2_CLIENT_ID = process.env.GATSBY_OAUTH2_CLIENT_ID;
 window.SCOPES = process.env.GATSBY_SCOPES;
