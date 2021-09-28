54beb496d6a9e002038adf17f87d62fe607427ee
sebastian marcet <smarcet@gmail.com>
2021-09-28 09:49:32 -0300

    Fixed duplicated events on schedule (#125)
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/actions/event-actions.js
+++ src/actions/event-actions.js
@@ -1,54 +1,58 @@
 import {
-  getAccessToken,
-  getRequest,
-  createAction,
-  stopLoading,
-  startLoading,
+    getAccessToken,
+    getRequest,
+    createAction,
+    stopLoading,
+    startLoading,
 } from 'openstack-uicore-foundation/lib/methods';
 
-import { customErrorHandler } from '../utils/customErrorHandler';
+import {customErrorHandler} from '../utils/customErrorHandler';
 
-import { LOGOUT_USER } from "openstack-uicore-foundation/lib/actions";
+import {LOGOUT_USER} from "openstack-uicore-foundation/lib/actions";
 
-export const GET_EVENT_DATA         = 'GET_EVENT_DATA';
-export const GET_EVENT_DATA_ERROR   = 'GET_EVENT_DATA_ERROR';
+export const GET_EVENT_DATA = 'GET_EVENT_DATA';
+export const GET_EVENT_DATA_ERROR = 'GET_EVENT_DATA_ERROR';
 
 export const handleResetReducers = () => (dispatch) => {
-  dispatch(createAction(LOGOUT_USER)({}));
+    dispatch(createAction(LOGOUT_USER)({}));
 };
 
 export const getEventById = (eventId) => async (dispatch, getState) => {
 
-  dispatch(startLoading());
+    dispatch(startLoading());
 
-  let { scheduleState: { allEvents } } = getState();
-  const event = allEvents.find(ev => ev.id === parseInt(eventId));
+    // check first the reducer
+    let {scheduleState: {allEvents}} = getState();
+    const event = allEvents.find(ev => ev.id === parseInt(eventId));
 
-  if (event) {
-    dispatch(createAction(GET_EVENT_DATA)({ event }));
-    dispatch(stopLoading());
-  } else {
+    if (event) {
+        dispatch(createAction(GET_EVENT_DATA)({event}));
+        dispatch(stopLoading());
+        return Promise.resolve();
+    }
+
+    // if does not exists get it from api
     const accessToken = await getAccessToken();
     if (!accessToken) {
-      dispatch(stopLoading());
-      return Promise.resolve();
+        dispatch(stopLoading());
+        return Promise.resolve();
     }
     let params = {
-      access_token: accessToken,
-      expand: 'track,location,location.venue,location.floor,speakers,slides,links,videos,media_uploads'
+        access_token: accessToken,
+        expand: 'track,location,location.venue,location.floor,speakers,slides,links,videos,media_uploads'
     };
-    
+
     return getRequest(
-      null,
-      createAction(GET_EVENT_DATA),
-      `${window.SUMMIT_API_BASE_URL}/api/v1/summits/${window.SUMMIT_ID}/events/${eventId}/published`,
-      customErrorHandler
+        null,
+        createAction(GET_EVENT_DATA),
+        `${window.SUMMIT_API_BASE_URL}/api/v1/summits/${window.SUMMIT_ID}/events/${eventId}/published`,
+        customErrorHandler
     )(params)(dispatch).then(() => {
-      dispatch(stopLoading());
+        dispatch(stopLoading());
     }).catch(e => {
-      dispatch(stopLoading());
-      dispatch(createAction(GET_EVENT_DATA_ERROR)(e));
-      return (e);
+        dispatch(stopLoading());
+        dispatch(createAction(GET_EVENT_DATA_ERROR)(e));
+        return (e);
     });
-  }
+
 };
--- src/reducers/schedule-reducer.js
+++ src/reducers/schedule-reducer.js
@@ -3,76 +3,79 @@ import eventsData from '../content/events.json';
 import filtersData from '../content/filters.json';
 import {syncFilters} from "../utils/filterUtils";
 import {getFilteredEvents} from '../utils/schedule';
-import { LOGOUT_USER } from "openstack-uicore-foundation/lib/actions";
-import { UPDATE_FILTER, UPDATE_FILTERS, CHANGE_VIEW } from '../actions/schedule-actions'
-import { RESET_STATE, SYNC_DATA } from '../actions/base-actions';
-import { GET_EVENT_DATA } from '../actions/event-actions';
+import {LOGOUT_USER} from "openstack-uicore-foundation/lib/actions";
+import {UPDATE_FILTER, UPDATE_FILTERS, CHANGE_VIEW} from '../actions/schedule-actions'
+import {RESET_STATE, SYNC_DATA} from '../actions/base-actions';
+import {GET_EVENT_DATA} from '../actions/event-actions';
 
 const {color_source, ...filters} = filtersData;
 
 const summitTimeZoneId = summitData.summit.time_zone_id;  // TODO use reducer data
 
 const DEFAULT_STATE = {
-  filters: filters,
-  colorSource: color_source,
-  events: eventsData,
-  allEvents: eventsData,
-  view: 'calendar'
+    filters: filters,
+    colorSource: color_source,
+    events: eventsData,
+    allEvents: eventsData,
+    view: 'calendar'
 };
 
 const scheduleReducer = (state = DEFAULT_STATE, action) => {
-  const { type, payload } = action;
+    const {type, payload} = action;
 
-  switch (type) {
-    case RESET_STATE:
-    case LOGOUT_USER:
-      return DEFAULT_STATE;
-    case SYNC_DATA: {
-      const {filters: currentFilters} = state;
-      // new filter could have new keys, or less keys that current one .... so its the source of truth
-      const newFilters =  syncFilters({...filters}, currentFilters);
-      const events = getFilteredEvents(eventsData, newFilters, summitTimeZoneId);
-      return {...state, allEvents: eventsData, filters:newFilters, colorSource: color_source, events};
+    switch (type) {
+        case RESET_STATE:
+        case LOGOUT_USER:
+            return DEFAULT_STATE;
+        case SYNC_DATA: {
+            const {filters: currentFilters} = state;
+            // new filter could have new keys, or less keys that current one .... so its the source of truth
+            const newFilters = syncFilters({...filters}, currentFilters);
+            const events = getFilteredEvents(eventsData, newFilters, summitTimeZoneId);
+            return {...state, allEvents: eventsData, filters: newFilters, colorSource: color_source, events};
+        }
+        case GET_EVENT_DATA: {
+            const {allEvents} = state;
+            // check first if we have api response
+            const event = payload.response || payload.event;
+            const index = state.allEvents.findIndex((e) => e.id === event.id);
+            const updatedEvents = [...allEvents];
+
+            if (index >= 0) {
+                // update the event on reducer
+                updatedEvents[index] = {...event};
+            } else {
+                // add the event to reducer
+                updatedEvents.push(event);
+            }
+            return {...state, allEvents: updatedEvents};
+        }
+        case UPDATE_FILTER: {
+            const {type, values} = payload;
+            const {filters, allEvents} = state;
+            filters[type].values = values;
+
+            // update events
+            const events = getFilteredEvents(allEvents, filters, summitTimeZoneId);
+
+            return {...state, filters, events}
+        }
+        case UPDATE_FILTERS: {
+            const {filters, view} = payload;
+            const {allEvents} = state;
+
+            // update events
+            const events = getFilteredEvents(allEvents, filters, summitTimeZoneId);
+
+            return {...state, filters, events, view}
+        }
+        case CHANGE_VIEW: {
+            const {view} = payload;
+            return {...state, view}
+        }
+        default:
+            return state;
     }
-    case GET_EVENT_DATA: {
-      const {allEvents} = state;
-      const event = payload.response || payload.event;
-      const index = state.allEvents.findIndex((e) => e.id === event.id);
-      const updatedEvents = [...allEvents];
-
-      if (index > 0) {
-        updatedEvents[index] = event;
-      } else {
-        updatedEvents.push(event);
-      }
-      return { ...state, allEvents: updatedEvents };
-    }
-    case UPDATE_FILTER: {
-      const {type, values} = payload;
-      const {filters, allEvents} = state;
-      filters[type].values = values;
-
-      // update events
-      const events = getFilteredEvents(allEvents, filters, summitTimeZoneId);
-
-      return {...state, filters, events}
-    }
-    case UPDATE_FILTERS: {
-      const {filters, view} = payload;
-      const {allEvents} = state;
-
-      // update events
-      const events = getFilteredEvents(allEvents, filters, summitTimeZoneId);
-
-      return {...state, filters, events, view}
-    }
-    case CHANGE_VIEW: {
-      const {view} = payload;
-      return {...state, view}
-    }
-    default:
-      return state;
-  }
 };
 
 export default scheduleReducer
