176c534bff3fff5d056a11452d29ab8c9bccc602
gcutrini <gabriel.cutrini@gmail.com>
2021-11-08 23:15:16 -0600

    Renders "Checking credentials" in authz callback route. (#201)
    * Renders "Checking credentials" in authz callback route.
    
    * Reimplements render as defined in abstract class but modifies the return (if no errors) to improve UX.

--- src/routes/authorization-callback-route.js
+++ src/routes/authorization-callback-route.js
@@ -19,6 +19,8 @@ import { connect } from 'react-redux';
 import { AbstractAuthorizationCallbackRoute } from "openstack-uicore-foundation/lib/components";
 import { getUserProfile } from '../actions/user-actions'
 
+import HeroComponent from "../components/HeroComponent";
+
 import { getEnvVariable, IDP_BASE_URL, OAUTH2_CLIENT_ID } from '../utils/envVariables'
 
 class AuthorizationCallbackRoute extends AbstractAuthorizationCallbackRoute {
@@ -38,6 +40,23 @@ class AuthorizationCallbackRoute extends AbstractAuthorizationCallbackRoute {
         error.includes('consent_required')
     ) return <Redirect to={'/'} noThrow />;
     return <Redirect to={`/error?error=${error}`} noThrow/>;
+  }
+
+  render() {
+    // reimplements same render as defined in abstract class
+    // but modifies the return (if no errors) to improve UX
+    // re: https://github.com/OpenStackweb/openstack-uicore-foundation/blob/cf8337911dcbb9d71bef3624c45256039e6447a0/src/components/security/abstract-auth-callback-route.js#L139
+    let {id_token_is_valid, error, error_description} = this.state;
+
+    if (error != null) {
+        console.log(`AbstractAuthorizationCallbackRoute::render _redirect2Error error ${error}`)
+        return this._redirect2Error(error);
+    }
+
+    if (!id_token_is_valid) {
+        return this._redirect2Error("token_validation_error");
+    }
+    return <HeroComponent title="Checking credentials..."/>;
   }
 }
 
