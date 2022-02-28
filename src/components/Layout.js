218b309b7bef1811b75b0714019ef86a970808ca
Camilo J. Kawerín <camilokawerin@gmail.com>
2022-02-28 11:07:32 -0300

    Feature/a11y skip to content (#273)
    * Added link to skip navigation and jump to content.
    
    * Removed ARIA attribute, added class name.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -77,6 +77,7 @@ const TemplateWrapper = ({ children, location, marketing, summit, favicons }) =>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
       </Helmet>
+      <a className="sr-only skip-to-content" href="#content-wrapper">Skip to content</a>
       <Header location={location} />
       <ClockComponent active={isFocus} summit={summit} />
       <div id="content-wrapper">{children}</div>
