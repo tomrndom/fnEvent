eff835cd9d8904051b8dbf1e52a7861428684bd9
gcutrini <gabriel.cutrini@gmail.com>
2022-02-28 10:21:52 -0300

    Fixes summit and event phase calculation. (#284)

--- src/utils/phasesUtils.js
+++ src/utils/phasesUtils.js
@@ -15,22 +15,22 @@ export const getSummitPhase = function (summit, now) {
 
   const deltaSummit = MarketingSite.summit_delta_start_time ? MarketingSite.summit_delta_start_time : 0;
 
-  return start_date - deltaSummit < now && end_date > now ? PHASES.DURING
+  return start_date - deltaSummit <= now && end_date > now ? PHASES.DURING
       :
       start_date - deltaSummit > now ? PHASES.BEFORE
         :
-        end_date < now ? PHASES.AFTER : null;
+        end_date <= now ? PHASES.AFTER : null;
 
 };
 
 export const getEventPhase = function (event, now) {
   const { start_date, end_date } = event;
 
-  return start_date < now && end_date > now ? PHASES.DURING
+  return start_date <= now && end_date > now ? PHASES.DURING
       :
       start_date > now ? PHASES.BEFORE
         :
-        end_date < now ? PHASES.AFTER : null;
+        end_date <= now ? PHASES.AFTER : null;
 };
 
 export const getVotingPeriodPhase = (votingPeriod, now) => {
