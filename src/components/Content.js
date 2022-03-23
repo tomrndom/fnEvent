df578ad2590daf9d968bd80a5b2d33dda7a6dd1e
Camilo J. Kawerín <camilokawerin@gmail.com>
2022-03-23 13:01:37 -0300

    Feature/a11y clean html from cms (#347)
    * Added module for HTML cleaning.
    
    * Clean HTML content using clean-html module.
    
    * Removed debugging line.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import cleaner from 'clean-html'
 
-export const HTMLContent = ({ content, className }) => (
-  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-)
+export const HTMLContent = ({ content, className }) => {
+  const [cleanHTML, setCleanHTML] = useState(content);
+  
+  useEffect(() => {
+    const options = {
+      'remove-empty-tags': ['a', 'p'],
+      'indent': '',
+      'break-around-tags': []
+    }
+    cleaner.clean(content, options, cleaned => {
+      setCleanHTML(cleaned)
+    })
+  }, [content])
+  
+  return (
+    <div className={className} dangerouslySetInnerHTML={{ __html: cleanHTML }} />
+  )
+}
 
 const Content = ({ content, className }) => (
   <div className={className}>{content}</div>
