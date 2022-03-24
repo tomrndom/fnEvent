3aebb0ef0fea72b2c1a4891d68e018f295b0859e
Jared Hill <jaredbhill@gmail.com>
2022-03-24 16:15:06 -0500

    chore: upgrade to gatsby v4

--- src/state/reduxPersistStorage.js
+++ src/state/reduxPersistStorage.js
@@ -5,7 +5,7 @@
  * @see https://github.com/vercel/next.js/discussions/15687#discussioncomment-45319
  */
 
-import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
 
 const createNoopStorage = () => {
   return {
@@ -21,6 +21,6 @@ const createNoopStorage = () => {
   };
 };
 
-const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
+const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
 
 export default storage;
