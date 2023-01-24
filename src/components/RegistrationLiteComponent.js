d2b75a4d2579b9aacb11155b880bb6c0bb8f327e
Josh Lohse <yoshwaaa@gmail.com>
2023-01-24 14:24:08 -0500

    Removed facebook while app is down

--- src/components/RegistrationLiteComponent.js
+++ src/components/RegistrationLiteComponent.js
@@ -71,7 +71,7 @@ const RegistrationLiteComponent = ({
         ];
 
         const thirdPartyProviders = [
-            { button_color: '#1877F2', provider_label: 'Continue with Facebook', provider_param: 'facebook', provider_logo: '../img/third-party-idp/logo_facebook.svg', provider_logo_size: 22 },
+            // { button_color: '#1877F2', provider_label: 'Continue with Facebook', provider_param: 'facebook', provider_logo: '../img/third-party-idp/logo_facebook.svg', provider_logo_size: 22 },
             { button_color: '#0A66C2', provider_label: 'Sign in with LinkedIn', provider_param: 'linkedin', provider_logo: '../img/third-party-idp/logo_linkedin.svg', provider_logo_size: 21 },
             // { button_color: '#000000', provider_label: 'Continue with Apple', provider_param: 'apple', provider_logo: '../img/third-party-idp/logo_apple.svg', provider_logo_size: 19 }
         ];
